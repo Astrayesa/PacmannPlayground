b9f8918d2f9e835107be1b3aea5c43ef2f97b2dc
Muhamad Sepryan Astrayesa <muhamad.sepryan1028@students.unila.ac.id>
2023-07-22 10:36:17 +0700

    edit hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
 # learn python debuger in vscode
-
 hello_str = "Hello world!"
-
-print(hello_str)
-
-input_str = input("Masukan string: ")
-
-print(input_str)
+hello_str = input("Masukan string: ")
